d07d1444dc26b27cc40f3874cc58e09286e70830
Ryan Vaznis <ryan.vaznis@gmail.com>
2020-02-11 14:09:57 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,4 +3,7 @@ def reverse_each_word (sentence)
   rev_sentence.collect { |rev_words| rev_words.reverse!}
   rev_sentence.join(" ")
 
+#  actual solution -- remember you can use method after {}
+#  sentence.split.collect {|word| word.reverse}.join(" ")
+
 end
